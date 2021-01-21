372b4cc208376c70f9c1fe4c93a677d99b91efea
Fredrik Arvas <fredrik.arvas@arbetsformedlingen.se>
2021-01-21 17:36:06 +0100

    Added Install all module for Secure Messages

---
+++ mail_install_modules_sm/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Af Secure Messages Install all modules",
+    "version": "12.0.1.0.1",
+    "author": "Swedish Public Employement Service",
+    "license": "AGPL-3",
+    "website": "https://vertel.se/",
+    "category": "Tools",
+    "depends": [
+		"contacts", 
+		"hr", 
+		"mail", 
+			],
+    "application": False,
+    "installable": True,
+}
+
