8ac67b65b8ed36838c3e56256f33dfac2943dc83
Fredrik Arvas <fredrik.arvas@arbetsformedlingen.se>
2021-02-03 10:16:39 +0100

    AFC-1725 Adds Mass mailing module

---
+++ mass_mail_template_af/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "AF Mass Mailing Template",
+    "summary": "Adds a Mass Mailing Template Design",
+    "version": "12.0.1.1.0",
+    "category": "Email Marketing",
+	"description": """
+	 v12.0.0.1 AFC-86  Add Mass mailing template for AF newsletters.\n
+	 v12.0.0.2  This version fixes bug in the template and makes the AF-template be visible beside the other templates.\n
+	 v12.0.1.1.0 Change namen
+	 
+    """,
+    "license": "AGPL-3",
+    "maintainer": "Swedish Public Employement Service",
+	"author": "Vertel AB",
+	"contributors": ['Swedish Public Employement Service', 'Vertel AB', 'Harhu Technologies Pvt. Ltd.'],
+    "depends": [
+        "mass_mailing"
+    ],
+    'data': [
+        "data/mail_template.xml",
+    ],
+    'installable': True,
+}
