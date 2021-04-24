a851050c81a8ab073c0abf2252780b42e9751857
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-04-24 09:39:17 +0200

    updated with mail_massmail_template_af

--- mail_install_modules_adkd/__manifest__.py
+++ mail_install_modules_adkd/__manifest__.py
@@ -8,9 +8,10 @@
     "website": "https://arbetsformedlingen.se/",
     "category": "Tools",
     "depends": [
-		"mail_autoresponder",		# generic module to process automatic newsletters
-		"mail_autoresponder_crm",	# CRM-addon to glue the module to CRM-specific views
-		"mail_browser_view",		# OCA module display the newsletter-email in the webbrowser
+		"mail_autoresponder",		# vertelab/odoo-mail generic module to process automatic newsletters
+		"mail_autoresponder_crm",	# vertelab/odoo-mail CRM-addon to glue the module to CRM-specific views
+		"mail_browser_view",		# vertelab/odoo-mail upgraded OCA module display the newsletter-email in the webbrowser
+	    	"mass_mailing_editor_af", 	# vertelab/odoo-mail Module that adds email-editor snippets with custom Af design
 			],
     "application": False,
     "installable": True,
